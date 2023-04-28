b3067b2bb41dcf29e74994a47f2781e8130e7a4d
Diogo-gallina <diogogallina20@gmail.com>
2023-04-28 20:59:48 -0300

    Iniciando js, const, let, var

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+// console.log("Olá Mundo");
+
+// var nr1 = "10";
+// var nr2 = "5";
+
+
+//Realizando operações com String
+
+//ParseInt convertendo string para inteiro
+// console.log(parseInt(nr1)+parseInt(nr2));
+
+var nome1 = "Dubas";
+// let nome2 = "Serbato";
+// const nome3 = "Julia";
+
+if(nome1 == ""){
+    let nome1 = "Julia"
+}
+
+console.log(nome1)
