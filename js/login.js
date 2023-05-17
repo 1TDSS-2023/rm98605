2bf3a28d8a0718d59986d2932b046e2ff0277d27
Diogo-Galllina <diogogallina20@gmail.com>
2023-05-17 19:49:01 -0300

    Criando forms e começando js do login

---
+++ js/login.js
@@ -0,0 +1,10 @@
+const input = document.querySelector("#idUsuario");
+const senha = document.querySelector("#idSenha");
+
+input.addEventListener("focus", ()=>{
+
+});
+
+senha.addEventListener("focus", ()=>{
+
+});
