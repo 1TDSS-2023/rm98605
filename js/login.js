57d897fecab82df3ed578844038b24ad30689aa9
Diogo-Galllina <diogogallina20@gmail.com>
2023-05-17 20:43:55 -0300

    Iniciando validações dos campos do login

--- js/login.js
+++ js/login.js
@@ -1,10 +1,27 @@
-const input = document.querySelector("#idUsuario");
-const senha = document.querySelector("#idSenha");
+//O que é um evento? 
+//Um evento é uma ação que ocorre seja ela feita, pelo usuário ou sistema: 
+//Ex: O sistema pode carregar a página, o usuário pode clicar no botão, o usuário pode pressionar a tecla, etc; 
+//atributo = style 
+//valor do atributo outline-color 
+// a cor do atributo = vermelho 
 
-input.addEventListener("focus", ()=>{
+const inputUser = document.querySelector("#idUsuario"); 
+const inputSenha = document.querySelector("#idSenha"); 
+const userLabel = document.querySelector("label[for='idUsuario']"); 
+const passLabel = document.querySelector("label[for='idSenha']"); 
+
+inputSenha.addEventListener("keyup", ()=>{ if(inputSenha.value.length < 5){ 
+    inputSenha.setAttribute("style", "outline-color: red;"); passLabel.setAttribute("style", "color: red;"); }else{ inputSenha.setAttribute("style", "outline-color: green;"); passLabel.setAttribute("style", "color: green;"); } }); 
+
+inputUser.addEventListener("keyup",()=>{ if(inputUser.value.length < 5){ inputUser.setAttribute("style", "outline-color: red;"); userLabel.setAttribute("style", "color: red;"); }else{ inputUser.setAttribute("style", "outline-color: green;"); userLabel.setAttribute("style", "color: green;"); } }); 
+
+const eyePass = document.querySelector(".fa-eye"); 
+eyePass.addEventListener("click", ()=>{ 
+    if(inputSenha.getAttribute("type") == "password"){
+        inputSenha.setAttribute("type", "text");
+    }else{
+        inputSenha.setAttribute("type", "password");
+    }
 
 });
 
-senha.addEventListener("focus", ()=>{
-
-});
